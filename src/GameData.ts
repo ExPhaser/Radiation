00174d12ad87b8575c4f4f527c0aadccb8056b23
ExPhaser <102687218+ExPhaser@users.noreply.github.com>
2022-04-02 12:38:08 +0200

    Update GameData.ts

--- src/GameData.ts
+++ src/GameData.ts
@@ -164,7 +164,8 @@ export let GameData: any = {
     { name: "cubes", path: "assets/images/cubes.png" },
     { name: "trasparenza", path: "assets/images/trasparenza.png" },
     { name: "nuke", path: "assets/images/nuke.png" },
-    { name: "bg3", path: "assets/images/bg3.png" }
+    { name: "bg3", path: "assets/images/bg3.png" },
+    { name: "bg_over", path: "assets/images/bg_over.png" }
 
     // Bosses
     { name: "Patient_Zero", path: "assets/images/PZ.png" },
