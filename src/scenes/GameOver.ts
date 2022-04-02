78eb59e13b612d3873253e5e8db3c74a80bc8d70
ExPhaser <102687218+ExPhaser@users.noreply.github.com>
2022-04-02 12:35:15 +0200

    Update GameOver.ts

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -4,6 +4,8 @@ export default class GameOver extends Phaser.Scene {
   private _intro: Phaser.GameObjects.BitmapText;
   private _restart: Phaser.GameObjects.BitmapText;
 
+  private _cubes: Phaser.GameObjects.TileSprite;
+
   constructor() {
     super({
       key: "GameOver",
@@ -11,38 +13,28 @@ export default class GameOver extends Phaser.Scene {
   }
 
   create() {
-    //localStorage.removeItem("score");
-    //localStorage.removeItem("lives");
-    //localStorage.removeItem("bestLevel");
-    this.cameras.main.setBackgroundColor("#000000");
+     this._cubes = this.add.tileSprite(0, 0, 1024, 600, "bg_over").setOrigin(0).setScale(2);
     let particles = this.add.particles('flares');
 
     particles.createEmitter({
-      frame: 'blue',
-      y: 0,
-      x: { min: 0, max: 1024 },
-      lifespan: 10000,
-      speedY: { min: 100, max: 300 },
-      scale: { start: 0.1, end: 0.1 },
-      quantity: 1
+        frame: 'blue',
+        y: 0,
+        x: { min: 0, max: 1024 },
+        lifespan: 10000,
+        speedY: { min: 100, max: 300 },
+        scale: { start: 0.1, end: 0.1 },
+        quantity: 1
     });
-
-    this._GameOver = this.add
-      .bitmapText(this.game.canvas.width / 2, 65, "arcade", "GAME OVER", 60)
-      .setAlpha(1)
-      .setOrigin(0)
-      .setDepth(1001)
-      .setOrigin(0.5).setTint(0xffffff);
-
+    
     this.add
-      .bitmapText(this.game.canvas.width / 2, 165, "arcade", "YOUR SCORE:" + this.registry.get("score"), 30)
+      .bitmapText(this.game.canvas.width/2, 310, "arcade", "YOUR SCORE:" + this.registry.get("score"), 30)
       .setAlpha(1)
       .setOrigin(0)
       .setDepth(1001)
       .setOrigin(0.5).setTint(0xffffff);
-
-    this._intro = this.add
-      .bitmapText(this.game.canvas.width - 140, 550, "arcade", "Intro")
+    
+      this._intro = this.add
+      .bitmapText(this.game.canvas.width-140, 550, "arcade", "Intro")
       .setAlpha(1)
       .setOrigin(0.5)
       .setInteractive()
@@ -59,8 +51,8 @@ export default class GameOver extends Phaser.Scene {
       .on("pointerout", () => {
         this._intro.setTint(0xff8200);
       });
-
-    this._restart = this.add
+    
+     this._restart = this.add
       .bitmapText(140, 550, "arcade", "Replay")
       .setAlpha(1)
       .setOrigin(0.5)
@@ -80,23 +72,23 @@ export default class GameOver extends Phaser.Scene {
 
   }
 
-  intro() {
+   intro() {
+  
+      this.scene.stop("GameOver");
+      this.scene.start("Intro");
+      
+   }
 
-    this.scene.stop("GameOver");
-    this.scene.start("Intro");
-
-  }
-
-  restartGame() {
-
-    this.scene.stop("GameOver");
-    this.scene.start("GamePlay");
-    this.scene.bringToTop("GamePlay");
-    this.scene.start("Hud");
-    this.scene.bringToTop("Hud");
-
-  }
-
-
+   restartGame() {
+  
+      this.scene.stop("GameOver");
+      this.scene.start("GamePlay");
+      this.scene.bringToTop("GamePlay");
+      this.scene.start("Hud");
+      this.scene.bringToTop("Hud");
+     
+   }
+  
+  
 
 }
